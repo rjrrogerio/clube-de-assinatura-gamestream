f1e02d690e5cad1d72755d6ad47bc0ccaa7ee449
Rogerio Vieira Da Silva Junior <rogerio.junior@sescsp.org.br>
2022-08-09 12:15:14 -0300

    add initial pages/create views

---
+++ gamestream/gamestream/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'about.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'cart.html', context)
+
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
+
+def register(request):
+    context = {}
+    return render(request, 'register.html', context)
